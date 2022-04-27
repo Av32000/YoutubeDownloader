c73b8cb646ab75b70b61390a828fd620932174d3
Av32000 <ae.av32000@gmail.com>
2022-04-27 14:31:25 +0200

    Remove App code

--- webapp.js
+++ webapp.js
@@ -41,30 +41,6 @@ app.post('/youtube', (req, res) => {
   }
 });
 
-app.post('/youtubeapp', (req, res) => {
-  const url = req.body.url;
-  if (!url) {
-    res.send('No url provided');
-    return;
-  }
-  try {
-    ytdl.getBasicInfo(url).then(async info => {
-      const title = info.videoDetails.title;
-      res.header('Content-Disposition', 'attachment; filename=' + title + '.mp4');
-
-      await ytdl(url, {
-        format: "mp4",
-        quality: "highestaudio",
-      }).pipe(fs.createWriteStream('./videos/' + title + '.mp4'));
-
-      res.send(title)
-
-    }).catch(err => { res.send(err) });
-  } catch (error) {
-    res.send(error);
-  }
-});
-
 app.listen(8082, () => {
   console.log('Server started on port 8082');
 });
